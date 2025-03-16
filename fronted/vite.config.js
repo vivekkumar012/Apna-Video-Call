c61ce24a6eb0fb397c9ad980eb0a04361724509d
Vivek Kumar <154126190+vivekkumar012@users.noreply.github.com>
2025-03-16 13:32:05 +0530

    Update vite.config.js

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -16,18 +16,18 @@ export default defineConfig({
   },
 });
 
-build: {
-  chunkSizeWarningLimit: 1000, // Increase limit (default is 500 KB)
-  rollupOptions: {
-    output: {
-      manualChunks(id) {
-        if (id.includes('node_modules')) {
-          const packages = id.toString().split('node_modules/')[1].split('/')[0];
-          return `vendor-${packages}`;
-        }
-      },
-    },
-  },
-}
+// build: {
+//   chunkSizeWarningLimit: 1000, // Increase limit (default is 500 KB)
+//   rollupOptions: {
+//     output: {
+//       manualChunks(id) {
+//         if (id.includes('node_modules')) {
+//           const packages = id.toString().split('node_modules/')[1].split('/')[0];
+//           return `vendor-${packages}`;
+//         }
+//       },
+//     },
+//   },
+// }
 
 
