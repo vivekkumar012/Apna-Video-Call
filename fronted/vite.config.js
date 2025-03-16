8908c0f1d02d83bebb04e8d5ad5fde9bdc238b66
Vivek Kumar <154126190+vivekkumar012@users.noreply.github.com>
2025-03-16 13:26:38 +0530

    Update vite.config.js

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -1,20 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-});
+})
+
+
