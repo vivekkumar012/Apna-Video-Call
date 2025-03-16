0a28ff18ca8f8e3d1010fff7096579e1db297eba
Vivek Kumar <vh118854@gmail.com>
2025-03-16 12:57:33 +0530

    Deploying fronted

--- fronted/src/environment.js
+++ fronted/src/environment.js
@@ -1,2 +1,6 @@
-const server = "http://localhost:8000";
+let IS_PROD = true;
+const server = IS_PROD ?
+     "https://apnaprojectbackend.onrender.com" :
+    "http://localhost:8000" 
+    
 export default server;
